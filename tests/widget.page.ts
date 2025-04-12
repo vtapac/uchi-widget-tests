24259652d2a739196ace5d50140175c6037fae29
Taras <vtapac@gmail.com>
2025-04-13 00:11:23 +0800

    Initial version with working widget tests

--- tests/widget.page.ts
+++ tests/widget.page.ts
@@ -1,11 +1,12 @@
-import {Page} from "@playwright/test";
+import { Page } from "@playwright/test";
 
 enum WidgetPageSelectors {
     WRAPPER = '.sc-dino-typography-h > [class^=widget__]',
     WIDGET_BODY = '[class^=widgetWrapper] > [class^=widget__]',
     HEADER_TEXT = 'header h5',
     BUTTON_OPEN = '[data-test=openWidget]',
-    BUTTON_WRITE_TO_US = '[class^=btn]',
+    BUTTON_CLOSE = '.closeBtn__UZ4Ws',
+    BUTTON_WRITE_TO_US = '[data-test=button_feedback_form]',
     ARTICLE_POPULAR_TITLE = '[class^=popularTitle__]',
     ARTICLE_POPULAR_LIST = `${ARTICLE_POPULAR_TITLE} + ul[class^=articles__]`,
     ARTICLE_POPULAR_LIST_ITEM = `${ARTICLE_POPULAR_LIST} > li`,
@@ -14,7 +15,7 @@ enum WidgetPageSelectors {
 export class WidgetPage {
     static selector = WidgetPageSelectors;
 
-    constructor(protected page: Page) {}
+    constructor(protected page: Page) { }
 
     wrapper() {
         return this.page.locator(WidgetPage.selector.WRAPPER)
@@ -23,13 +24,19 @@ export class WidgetPage {
     async openWidget() {
         return this.wrapper().locator(WidgetPage.selector.BUTTON_OPEN).click();
     }
-
+    async closeWidget() {
+        return this.wrapper().locator(WidgetPage.selector.BUTTON_CLOSE).click();
+    }
     async getPopularArticles() {
-        return this.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_LIST_ITEM).all()
+        const locator = this.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_LIST_ITEM);
+        await locator.first().waitFor();
+        return locator.all();
     }
 
     async clickWriteToUs() {
-        return this.wrapper().locator(WidgetPage.selector.BUTTON_WRITE_TO_US).click();
+        const button = this.wrapper().locator(WidgetPage.selector.BUTTON_WRITE_TO_US);
+        await button.waitFor(); // ждём, когда она реально появится в DOM
+        await button.click();   // теперь клик
     }
 
     async getTitle() {
--- tests/widget.spec.ts
+++ tests/widget.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-import {WidgetPage} from "./widget.page";
+import { WidgetPage } from "./widget.page";
 
 test.describe('Uchi.ru widget ', () => {
   let widgetPage: WidgetPage;
 
-  test.beforeEach(async ({page}) => {
+  test.beforeEach(async ({ page }) => {
     widgetPage = new WidgetPage(page);
 
     // open uchi.ru main page
@@ -14,7 +14,7 @@ test.describe('Uchi.ru widget ', () => {
     await page.click('._UCHI_COOKIE__button');
   });
 
-  test('opens', async ({page}) => {
+  test('opens', async ({ page }) => {
     await widgetPage.openWidget();
 
     await expect(widgetPage.getWidgetBody()).toBeVisible()
@@ -24,11 +24,24 @@ test.describe('Uchi.ru widget ', () => {
     await widgetPage.openWidget();
 
     const articles = await widgetPage.getPopularArticles();
-
+    expect(articles.length).toBeGreaterThan(0);
     await articles[0].click();
 
     await widgetPage.clickWriteToUs();
 
     expect(await widgetPage.getTitle()).toEqual('Связь с поддержкой');
+  });
+
+  test('closes widget', async ({ page }) => {
+    await widgetPage.openWidget();
+
+    // Проверяем, что виджет открылся
+    await expect(widgetPage.getWidgetBody()).toBeVisible();
+
+    // Кликаем по кнопке закрытия
+    await widgetPage.closeWidget();
+
+    // Проверяем, что виджет закрылся
+    await expect(widgetPage.getWidgetBody()).not.toBeVisible();
   });
 });
